7b4ccc9a9c86da9f91436020e7578b62cf6fc7a1
agasaroballet2023-alt <agasaroballet2023@gmail.com>
2026-08-02 11:30:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,9 +48,15 @@ window.onload = function() {
 };
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
+const closeMenu = document.querySelector('.close-menu');
 
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+closeMenu.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+});
+
 
 
